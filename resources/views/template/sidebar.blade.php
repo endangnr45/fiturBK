3ed9cbd51227ccb48246dc3925f235a7fc9317fd
FakhranaAlmas <fakhranaalmassy@gmail.com>
2023-02-28 20:50:13 +0700

    merubah side bar dan navbar

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -2,22 +2,13 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
-        </div>
+            <img src="image/logo-unair-removebg-preview.png" style="width: 1cm">
         <div class="sidebar-brand-text mx-3">SK SI</div>
     </a>
 
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
-
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
-
+    
     <!-- Divider -->
     <hr class="sidebar-divider">
 
@@ -26,12 +17,56 @@
         Kurikulum
     </div>
 
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-book"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-server"></i>
+            <span>Profil Prodi</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-archive"></i>
+            <span>Kurikulum</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-book-open"></i>
+            <span>Mata Kuliah</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-user"></i>
+            <span>Profil Lulusan</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-clipboard"></i>
+            <span>CPL SN-DIKTI</span></a>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-fw fa-clipboard"></i>
+            <span>CPL Prodi</span></a>
+    </li>
+
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
         <a class="nav-link" href="/bk">
-            <i class="fas fa-fw fa-cog"></i>
+            <i class="fas fa-fw fa-archive"></i>
             <span>Bahan Kajian</span></a>
     </li>
+
     {{-- <li class="nav-item">
         <a class="nav-link collapsed" href="/bk" data-toggle="collapse" data-target="#collapseTwo"
             aria-expanded="true" aria-controls="collapseTwo">
@@ -48,7 +83,7 @@
     </li> --}}
 
     <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
+    {{-- <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
             aria-expanded="true" aria-controls="collapseUtilities">
             <i class="fas fa-fw fa-wrench"></i>
@@ -64,18 +99,16 @@
                 <a class="collapse-item" href="utilities-other.html">Other</a>
             </div>
         </div>
-    </li>
+    </li> --}}
 
     <!-- Divider -->
-    <hr class="sidebar-divider">
+
 
     <!-- Heading -->
-    <div class="sidebar-heading">
-        Addons
-    </div>
+
 
     <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item ">
+    {{-- <li class="nav-item ">
         <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
             aria-controls="collapsePages">
             <i class="fas fa-fw fa-folder"></i>
@@ -94,28 +127,28 @@
                 <a class="collapse-item " href="blank.html">Blank Page</a>
             </div>
         </div>
-    </li>
+    </li> --}}
 
     <!-- Nav Item - Charts -->
-    <li class="nav-item">
+    {{-- <li class="nav-item">
         <a class="nav-link" href="charts.html">
             <i class="fas fa-fw fa-chart-area"></i>
             <span>Charts</span></a>
-    </li>
+    </li> --}}
 
     <!-- Nav Item - Tables -->
-    <li class="nav-item">
+    {{-- <li class="nav-item">
         <a class="nav-link" href="tables.html">
             <i class="fas fa-fw fa-table"></i>
             <span>Tables</span></a>
-    </li>
+    </li> --}}
 
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
 
     <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
+    {{-- <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
+    </div> --}}
 
 </ul>
